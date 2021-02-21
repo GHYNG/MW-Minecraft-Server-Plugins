bf55d4fa8b71ee04bd52c02462bc7e2f7257a799
GHYNG <47288986+GHYNG@users.noreply.github.com>
2021-02-21 08:22:38 +0800

    First upload

---
+++ org/mwage/mcPlugin/player_death_message/Main.java
@@ -0,0 +1,26 @@
+package org.mwage.mcPlugin.player_death_message;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.mwage.mcPlugin.main.Main_GeneralMethods;
+public class Main extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(new PlayerDeathListener(), this);
+	}
+}
+class PlayerDeathListener implements Listener, Main_GeneralMethods {
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		String name = player.getName();
+		Location location = player.getLocation();
+		String worldName = location.getWorld().getName();
+		int x = (int)location.getX(), y = (int)location.getY(), z = (int)location.getZ();
+		serverSay(line(name, "死于了", worldName, "世界，坐标为(", x, ", ", y, ", ", z, ")，", "祝您下次更幸运！"));
+	}
+}
