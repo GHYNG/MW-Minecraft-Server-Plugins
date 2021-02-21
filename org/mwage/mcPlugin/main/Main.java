3d6d40c43a54146959783e564425a28e9a5b23fc
GHYNG <47288986+GHYNG@users.noreply.github.com>
2021-02-21 08:23:26 +0800

    Add more methods

--- org/mwage/mcPlugin/main/Main.java
+++ org/mwage/mcPlugin/main/Main.java
@@ -9,47 +9,36 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Main_GeneralMethods {
 	@Override
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(new MainListener(), this);
 	}
 }
-class MainListener implements Listener {
+class MainListener implements Listener, Main_GeneralMethods {
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		sendWelcomeTitle(player);
 		sendWelcomeMessage(player);
 		sendWelcomeSound(player);
+		String name = player.getName();
+		serverSay(line("欢迎，", name, "！"));
 	}
 	@SuppressWarnings("deprecation")
 	public void sendWelcomeTitle(Player player) {
 		String title = ChatColor.AQUA + "欢迎来到Mwage奶路星起地！";
-		String subTitle = ChatColor.DARK_PURPLE + "愿星河与你同在， " + player.getName();
+		String subTitle = ChatColor.DARK_PURPLE + "愿星河与你同在，" + player.getName();
 		player.sendTitle(title, subTitle);
 	}
 	public void sendWelcomeMessage(Player player) {
 		String line0 = ChatColor.AQUA + "欢迎来到奶路服务器！";
-		String line1 = ChatColor.AQUA + "腐竹特别提醒： 请不要在主世界挖矿哦~";
+		String line1 = ChatColor.AQUA + "腐竹特别提醒：请不要在主世界挖矿哦~";
 		String line2 = ChatColor.AQUA + "如果想挖矿，可以通过/warp mine去矿界（资源界）挖矿哦！";
-		player.sendMessage(lines(line0, line1, line2));
+		player.sendMessage(page(line0, line1, line2));
 	}
 	public void sendWelcomeSound(Player player) {
 		Location location = player.getLocation();
 		player.playSound(location, Sound.ENTITY_PLAYER_LEVELUP, SoundCategory.PLAYERS, 0.8f, 1.2f);
 	}
-	public static String lines(String... lines) {
-		int length = lines.length;
-		if(length == 0) {
-			return "";
-		}
-		String content = "";
-		for(int i = 0; i < length - 1; i++) {
-			content += lines[i] + "\n";
-		}
-		content += lines[length - 1];
-		return content;
-	}
-	
 }
---
+++ org/mwage/mcPlugin/main/Main_GeneralMethods.java
@@ -0,0 +1,80 @@
+package org.mwage.mcPlugin.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.server.ServerCommandEvent;
+
+/*
+ * This interface contains methods relate to server generally,
+ * like broadcast messages.
+ * Also contains other tool methods, like those related to String.
+ */
+public interface Main_GeneralMethods {
+	/*
+	 * To connect string parts to a new string.
+	 */
+	default String line(Object... parts) {
+		String line = "";
+		for(Object part : parts) {
+			line += part;
+		}
+		return line;
+	}
+	/*
+	 * To connect lines to a page, separated by \n.
+	 */
+	default String page(Object... lines) {
+		int length = lines.length;
+		if(length == 0) {
+			return "";
+		}
+		String page = "";
+		for(int i = 0; i < length - 1; i++) {
+			page += lines[i] + "\n";
+		}
+		page += lines[length - 1];
+		return page;
+	}
+	/*
+	 * Logic NOT.
+	 */
+	default boolean not(boolean b) {
+		return !b;
+	}
+	/*
+	 * Logic AND.
+	 */
+	default boolean and(boolean... bs) {
+		boolean r = true;
+		for(boolean b : bs) {
+			r &= b;
+		}
+		return r;
+	}
+	/*
+	 * logic OR.
+	 */
+	default boolean or(boolean... bs) {
+		boolean r = false;
+		for(boolean b : bs) {
+			r |= b;
+		}
+		return r;
+	}
+	/*
+	 * Logic NAND.
+	 */
+	default boolean nand(boolean... bs) {
+		return not(and(bs));
+	}
+	/*
+	 * Logic NOR.
+	 */
+	default boolean nor(boolean... bs) {
+		return not(or(bs));
+	}
+	default void serverSay(String message) {
+		String command = "say " + message;
+		ServerCommandEvent event = new ServerCommandEvent(Bukkit.getConsoleSender(), command);
+		Bukkit.getServer().getPluginManager().callEvent(event);
+	}
+}
