e49f8335edf9933045ca5f8e64c311d19af76434
GHYNG <47288986+GHYNG@users.noreply.github.com>
2021-02-23 02:32:05 +0800

    Add more support methods.

--- org/mwage/mcPlugin/main/Main_GeneralMethods.java
+++ org/mwage/mcPlugin/main/Main_GeneralMethods.java
@@ -1,4 +1,6 @@
 package org.mwage.mcPlugin.main;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.event.server.ServerCommandEvent;
 /*
@@ -11,6 +13,13 @@ public interface Main_GeneralMethods {
 	 * To connect string parts to a new string.
 	 */
 	default String line(Object... parts) {
+		String line = "";
+		for(Object part : parts) {
+			line += part;
+		}
+		return line;
+	}
+	default String lineList(List<Object> parts) {
 		String line = "";
 		for(Object part : parts) {
 			line += part;
@@ -30,6 +39,18 @@ public interface Main_GeneralMethods {
 			page += lines[i] + "\n";
 		}
 		page += lines[length - 1];
+		return page;
+	}
+	default String pageList(List<Object> lines) {
+		int length = lines.size();
+		if(length == 0) {
+			return "";
+		}
+		String page = "";
+		for(int i = 0; i < length - 1; i++) {
+			page += lines.get(i) + "\n";
+		}
+		page += lines.get(length - 1);
 		return page;
 	}
 	/*
@@ -80,5 +101,24 @@ public interface Main_GeneralMethods {
 		if(!event.isCancelled()) {
 			Bukkit.broadcastMessage(message);
 		}
+	}
+	default <T> List<T> getNewList() {
+		List<T> list = new ArrayList<T>();
+		return list;
+	}
+	default boolean goodIdentifier(String identifier) {
+		if(identifier == null || identifier.length() < 1) {
+			return false;
+		}
+		int index = 0;
+		if(Character.isJavaIdentifierStart(identifier.charAt(index))) {
+			while(++index < identifier.length()) {
+				if(!Character.isJavaIdentifierPart(identifier.charAt(index))) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
 	}
 }
