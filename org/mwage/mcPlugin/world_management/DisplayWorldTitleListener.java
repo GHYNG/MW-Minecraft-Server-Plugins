f5704ce6cccdfc6ffe87031dca820e832842d9a3
GHYNG <1178391518@qq.com>
2021-07-16 16:12:42 -0400

    Update resource world name to "mine3". Some functions rewritten.
    Signed-off-by: GHYNG <1178391518@qq.com>

--- org/mwage/mcPlugin/world_management/DisplayWorldTitleListener.java
+++ org/mwage/mcPlugin/world_management/DisplayWorldTitleListener.java
@@ -10,23 +10,32 @@ import org.bukkit.event.player.PlayerChangedWorldEvent;
 public class DisplayWorldTitleListener implements Listener, MWWorldData {
 	private final Map<String, String> name_title = new HashMap<String, String>();
 	{
-		name_title.put(MAIN_WORLD_NAME, MAIN_WORLD_TITLE);
-		name_title.put(MAIN_NETHER_WORLD_NAME, MAIN_NETHER_WORLD_TITLE);
-		name_title.put(MAIN_END_WORLD_NAME, MAIN_END_WORLD_TITLE);
-		name_title.put(RESOURCE_WORLD_NAME, RESOURCE_WORLD_TITLE);
+		registerWorldTitle(MAIN_WORLD_NAME, MAIN_WORLD_TITLE);
+		registerWorldTitle(MAIN_NETHER_WORLD_NAME, MAIN_NETHER_WORLD_TITLE);
+		registerWorldTitle(MAIN_END_WORLD_NAME, MAIN_END_WORLD_TITLE);
+		registerWorldTitle(RESOURCE_WORLD_NAME, RESOURCE_WORLD_TITLE);
 	}
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onPlayerChangeWorld(PlayerChangedWorldEvent event) {
 		Player player = event.getPlayer();
 		World world = player.getWorld();
-		String name = name_title.get(world.getName());
-		name = name == null ? world.getName() : name;
+		String name = getWorldTitle(world.getName());
 		name = ChatColor.YELLOW + name;
 		player.sendTitle(name, ChatColor.YELLOW + "与世界同步完成， 武运昌隆！");
 		if(world.getName().equalsIgnoreCase(RESOURCE_WORLD_NAME)) {
 			String line0 = ChatColor.GOLD + "欢迎来到资源世界， 请不要不设防护的垂直挖矿哦。";
 			player.sendMessage(line0);
 		}
+	}
+	public void registerWorldTitle(String worldName, String worldTitle) {
+		if(worldName == null) {
+			return;
+		}
+		name_title.put(worldName.toLowerCase(), worldTitle);
+	}
+	public String getWorldTitle(String worldName) {
+		String title = name_title.get(worldName.toLowerCase());
+		return title == null ? worldName : title;
 	}
 }
