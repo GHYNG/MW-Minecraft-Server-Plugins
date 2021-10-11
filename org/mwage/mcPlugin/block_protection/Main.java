31a7eee4f7f4d4f6e94d9beab604eb2028710268
GHYNG <1178391518@qq.com>
2021-10-11 06:38:33 -0400

    展示框不会被玩家放置的方块挤掉。
    Signed-off-by: GHYNG <1178391518@qq.com>

--- org/mwage/mcPlugin/block_protection/Main.java
+++ org/mwage/mcPlugin/block_protection/Main.java
@@ -1,15 +1,20 @@
 package org.mwage.mcPlugin.block_protection;
+import java.util.Collection;
 import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.AbstractVillager;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.hanging.HangingBreakEvent;
@@ -19,6 +24,7 @@ public class Main extends JavaPlugin {
 	public void onEnable() {
 		Bukkit.getPluginManager().registerEvents(new ExplosionListener(), this);
 		Bukkit.getPluginManager().registerEvents(new EntityListener(), this);
+		Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
 	}
 }
 class ExplosionListener implements Listener {
@@ -37,11 +43,11 @@ class ExplosionListener implements Listener {
 	@EventHandler
 	public void onHangingBreak(HangingBreakEvent event) {
 		switch(event.getCause()) {
-			case EXPLOSION :
-				event.setCancelled(true);
+			case DEFAULT :
+			case ENTITY :
 				return;
 			default :
-				return;
+				event.setCancelled(true);
 		}
 	}
 }
@@ -62,5 +68,21 @@ class EntityListener implements Listener {
 			return;
 		}
 		event.setCancelled(true);
+	}
+}
+class PlayerListener implements Listener {
+	@EventHandler
+	public void onPlayerPlaceBlock(BlockPlaceEvent event) {
+		Block block = event.getBlock();
+		Location location = block.getLocation().toCenterLocation();
+		Collection<Entity> entities = location.getNearbyEntities(0.5, 0.5, 0.5);
+		entities.forEach((entity) -> {
+			if(entity instanceof ItemFrame) {
+				Player player = event.getPlayer();
+				player.sendMessage("展示框所在的位置上无法放置方块！");
+				event.setCancelled(true);
+				return;
+			}
+		});
 	}
 }
